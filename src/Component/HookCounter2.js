e65ea86f43f8b9a86f994c9a488036d37805bf7b
devpop <impopcity@gmail.com>
2022-09-28 10:06:37 +0530

    correct way

--- src/Component/HookCounter2.js
+++ src/Component/HookCounter2.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 function HookCounter2() {
     const initialcount = 0
-    const [count,setCount] = useState(initialcount)
+    const[count,setCount] = useState(initialcount)
+
+
+    const incrementnum = ()=>{
+        for(let i=0;i<5;i++){
+            setCount(prevCount=>prevCount+1)
+        }
+    }
   return (
     <div>
-        count:{count}<br></br>
-        <button onClick={()=>setCount(initialcount)}>reset</button>
-        <button onClick={()=>setCount(count+1)}>increment</button>
-        <button onClick={()=>setCount(count-1)}>decrement</button>
+        COUNT : {count}
+      <button onClick={()=>setCount(initialcount)}>RESET</button>
+      <button onClick={()=>setCount(prevCount=>prevCount+1)}>INCREMENT</button>
+      <button onClick={()=>setCount(prevCount=>prevCount-1)}>DECREMENT</button>
+      <button onClick={incrementnum}>INCREMENT 5</button>
     </div>
   )
 }
