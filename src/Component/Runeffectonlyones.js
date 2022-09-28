2819af3a124a5c048c3ab6935bc168695c6ac998
devpop <impopcity@gmail.com>
2022-09-28 14:30:56 +0530

    only ones

---
+++ src/Component/Runeffectonlyones.js
@@ -0,0 +1,30 @@
+import React,{useState,useEffect} from 'react'
+
+function Runeffectonlyones() {
+  const [x,setX] = useState(0)
+  const [y,setY] = useState(0)
+ 
+
+  const logMousePosition = (e)=>{
+    console.log("mouse event");
+    setX(e.clientX)
+    setY(e.clientY)
+  }
+
+
+
+  useEffect(()=>{
+    console.log("useeffect called");
+    window.addEventListener("mousemove",logMousePosition)
+  },[])
+
+
+
+  return (
+    <div>
+       HOOK X - {x} AND HOOK Y - {y}
+    </div>
+  )
+}
+
+export default Runeffectonlyones
