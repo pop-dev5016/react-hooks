2c0572ef341d35315a892c4c3c40690399d81120
devpop <impopcity@gmail.com>
2022-09-28 13:05:43 +0530

    use effect basic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './App.css';
-import Array4hook from './Component/Array4hook';
+import Effecthook from './Component/Effecthook';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Array4hook></Array4hook>
+   <Effecthook></Effecthook>
     </div>
   );
 }
