d49d916b34574f1c4d75fad1b7b008ef0eac4ff2
devpop <impopcity@gmail.com>
2022-09-28 11:02:10 +0530

    object merge

--- src/Component/Hookcounter3.js
+++ src/Component/Hookcounter3.js
@@ -7,11 +7,12 @@ function Hookcounter3() {
   return (
     <div>
       <form>
-        <input type="text" value={name.firstname} onChange={e =>setName({firstname:e.target.value})}/>
-        <input type="text" value={name.lastname} onChange={e =>setName({lastname: e.target.value})}/>
+        <input type="text" value={name.firstname} onChange={e =>setName({...name,firstname:e.target.value})}/>
+        <input type="text" value={name.lastname} onChange={e =>setName({...name,lastname: e.target.value})}/>
         
         <h1>your first name is -{name.firstname}</h1>
         <h1>your first name is -{name.lastname}</h1>
+        <h2>{JSON.stringify(name)}</h2>
       </form>
     </div>
   )
