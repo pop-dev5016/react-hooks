23db6b453b98cf98f6b735f270eb8d86e14d4dba
devpop <impopcity@gmail.com>
2022-09-28 21:59:55 +0530

    method 2

--- src/Component/Reducercounter2.js
+++ src/Component/Reducercounter2.js
@@ -5,9 +5,9 @@ const initialState = {firstCounter:0}
 const reducer = (currentstate,action)=>{
      switch(action.type){
         case 'increment':
-            return {firstCounter:currentstate.firstCounter+1}
+            return {firstCounter:currentstate.firstCounter+action.value}
         case 'decrement':
-            return {firstCounter:currentstate.firstCounter-1}
+            return {firstCounter:currentstate.firstCounter-action.value}
         case 'reset':
             return initialState
         default:
@@ -20,8 +20,10 @@ function ReducerCounter2() {
   return (
     <div>
         <div>count = {count.firstCounter}</div>
-     <button onClick={()=>dispatch({type:'increment'})}>Increment</button>
-     <button onClick={()=>dispatch({type:'decrement'})}>Decrement</button>
+     <button onClick={()=>dispatch({type:'increment',value:1})}>Increment</button>
+     <button onClick={()=>dispatch({type:'decrement',value:1})}>Decrement</button>
+     <button onClick={()=>dispatch({type:'increment',value:5})}>Increment 5</button>
+     <button onClick={()=>dispatch({type:'decrement',value:5})}>Decrement 5</button>
      <button onClick={()=>dispatch({type:'reset'})}>Reset</button>
     </div>
   )
