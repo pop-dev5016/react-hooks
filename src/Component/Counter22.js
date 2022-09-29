1efea20d2e4249eb6bab22a9969bf1c20f228b29
devpop <impopcity@gmail.com>
2022-09-29 18:07:05 +0530

    passing para

--- src/Component/Counter22.js
+++ src/Component/Counter22.js
@@ -2,7 +2,7 @@ import React,{useState}from 'react'
 import UseCounter from './hooks/UseCounter'
 
 function Counter22() {
-   const [count,increment,Decncrement,Reset]=UseCounter()
+   const [count,increment,Decncrement,Reset]=UseCounter(10)
   return (
     <div>
         <h2>count = {count}</h2>
--- src/Component/hooks/UseCounter.js
+++ src/Component/hooks/UseCounter.js
@@ -1,7 +1,7 @@
 import React ,{useState} from 'react'
 
-function UseCounter() {
-    const [count,setCount] = useState(0) 
+function UseCounter(initialCount=0) {
+    const [count,setCount] = useState(initialCount) 
     const increment = ()=>{
         setCount(prevCount=>prevCount+1)
     }
