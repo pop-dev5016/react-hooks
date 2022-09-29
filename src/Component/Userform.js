2098a24d8b3df479f4b937995f75d4c0c0669b0c
devpop <impopcity@gmail.com>
2022-09-29 19:36:24 +0530

    useform

--- src/Component/Userform.js
+++ src/Component/Userform.js
@@ -1,24 +1,28 @@
-import React,{useState}from 'react'
+import React,{useState} from 'react'
+import useInput from './hooks/useInput'
+
 
 function Userform() {
     
     const [firstName,bindFirstName,resetFirstName] = useInput('')
     const [lastName,bindLastName,resetLastName] = useInput('')
 
-    const submithandler = e=>{
+    const submithandler = e =>{
         e.preventDefault()
         alert(`hello ${firstName} ${lastName}`)
+        resetFirstName()
+        resetLastName()
     }
 
   return (
     <div>
         <form onSubmit={submithandler}>
             <label>first name: </label>
-            <input type="text" value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
+            <input type="text" {...bindFirstName} />
             <div>
                 <br></br>
             <label>last name: </label>
-            <input type="text" value={firstName} onChange={(e)=>setFirstName(e.target.value)}/>
+            <input type="text" {...bindLastName} />
             
             <div>
             <br></br>
